052977dbb5399ba1cd13defa7b388fa1f152f1f7
RickAllstar <henriquequirino891@gmail.com>
2024-03-01 23:57:32 -0300

    Update exercicios

---
+++ exercicios/exercicio07.rb
@@ -0,0 +1,12 @@
+#estrutura condicional
+7.odd?() # verifica se 7 é ímpar e retorna true
+4.even?() # verifica se 4 é par e retorna true
+#if else
+numero = 1
+outro_numero = 20
+
+if numero == outro_numero 
+  puts 'Os números são iguais'
+else
+  puts 'Os números são diferentes'
+end
