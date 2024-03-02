90708f66962d94c9c57d9ddb8a7d2c0fba7a72e8
RickAllstar <henriquequirino891@gmail.com>
2024-03-01 23:26:18 -0300

    Create exercicio06.rb

---
+++ exercicios/exercicio06.rb
@@ -0,0 +1,19 @@
+#Hash
+aluno = { nome: 'João', nota: 7, disciplina: 'Ciências' }
+puts aluno[:nome]
+puts aluno[:disciplina]
+aluno[:nome] = 'Maria'
+puts aluno[:nome]
+
+
+# Saiba que você pode encadear chamadas de arrays e hashes:
+alunos = [{ nome: 'Alberto', nota: 7, disciplina: 
+'Artes' }, { nome: 'Ingrid', nota: 10, disciplina: 'Biologia' }]
+# Posição 0 do Array chave nome do Hash
+alunos[0][:nome]
+
+
+#symbols
+# Os symbols são normalmente usados quando precisamos de identificadores. 
+“caneca” # string 
+:caneca  # symbol
